6c1afd4505130ec42d66fa6600f6ab01251ea213
ResonantWave <nixijav@nixijav.com>
2015-10-14 20:59:29 +0200

    Forgot to commit main.py. Oops :3

--- main.py
+++ main.py
@@ -3,23 +3,20 @@ import sys
 import re
 import urllib2
 import json
-import subprocess
 import time
 import wikipedia
 import logging
 import os
+import pyjokes
+import modules.list as commandList
+
 from websocket import create_connection
 
-## COMMAND IMPORTS
-import modules.translate as translate
-import modules.wiki as wiki
-import modules.weather as weather
-##
 
-
+#########################################
 TOKEN = "" # YOUR SLACK API KEY GOES HERE
 TOKEN = os.environ['TOKEN'] # DELETE THIS
-
+#########################################
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -40,20 +37,13 @@ def send(channel, text, type = 'message', id = '1'):
 while True:
    result = json.loads(ws.recv())
    try:
-      if result['type'] == "message":
-         if result['text'].lower() == "joke":
-            proc = subprocess.Popen(['pyjoke'], stdout=subprocess.PIPE, shell=True)
-            (out, err) = proc.communicate()
-	    send(result['channel'], out.replace('\n', ''))
+      if result['type'] == 'message':
+         if result['text'].lower() == 'joke':
+	    send(result['channel'], pyjokes.get_joke())
 
-         if result['text'].lower().find("wiki") != -1:
-            send(result['channel'], wiki.execute(result['text']))
-
-         if result['text'].lower().find("translate") != -1:
-            send(result['channel'], translate.execute(result['text']))
-
-         if result['text'].lower().find("weather") != -1:
-	    send(result['channel'], weather.execute(result['text']))
+	 if result['text'].lower().split()[0] in commandList.commandModules:
+	    function = commandList.commandModules[result['text'].lower().split()[0]]
+	    send(result['channel'], function.execute(result['text']))
       time.sleep(1)
    except KeyError:
       pass
